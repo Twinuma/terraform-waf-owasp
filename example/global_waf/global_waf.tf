a283fd907c350b17e6198e61ca809a9c5738b98a
Vlad Holubiev <golubev.vld@gmail.com>
2018-08-15 17:21:16 +0300

    feat: add web_acl_id to module outputs so it can be attached elsewhere

--- example/global_waf/global_waf.tf
+++ example/global_waf/global_waf.tf
@@ -2,3 +2,6 @@ module "global_waf" {
   source = "../../global/."
   waf_prefix = "${var.waf_prefix}"
 }
+
+# Use module.global_waf.web_acl_id variable to attach it to aws_cloudfront_distribution
+# https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#web_acl_id
